e3841f0f94861ae2490b5d45c2e897317f4be974
gabrielsaltonn <gbl.salton@gmail.com>
2025-11-19 06:14:12 -0300

    ex011

---
+++ Módulo D/ex008.js
@@ -0,0 +1,6 @@
+var vel = 120
+console.log(`A velocidade do seu veículo é de ${vel}Km/h`)
+if (vel > 60) {
+    console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Use o cinto de segurança!`)
