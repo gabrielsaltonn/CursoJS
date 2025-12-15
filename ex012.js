ad8c994248fdc9127c495788767c5cfaad3294d9
Gabriel Saton <gbl.salton@gmail.com>
2025-12-15 16:32:31 -0300

    aula12

---
+++ ex012.js
@@ -0,0 +1,12 @@
+var agora = new Date()
+var hora = agora.getHours()
+console.log (`Agora são exatamente ${hora} horas.`)
+if (hora < 5) {
+    console.log(`Madrugada!`)
+} else if (hora < 12) {
+    console.log(`Bom dia!`)
+} else if (hora < 19) {
+    console.log(`Boa tarde!`)
+} else if (hora < 24) {
+    console.log(`Boa noite!`)
+}
